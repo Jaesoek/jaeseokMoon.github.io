ea0ff7d2588feb95a25a0e8ff49479b63435e07d
Jaeseok <answo93@1thefull.com>
2023-01-24 17:42:26 +0900

    스크롤 기능 1차 구현 및 Service 창 배치 완료

--- js/service.js
+++ js/service.js
@@ -1,9 +1,9 @@
 const li = document.querySelectorAll(".move");
-const navPos = document.querySelectorAll("section");
+const navPos = document.querySelectorAll(".nav_point");
 
 function activeMenu() {
     let len = navPos.length
-    while(--len && window.scrollY + 97 < navPos[len].offsetTop) {}
+    while(--len && window.scrollY + 500 < navPos[len].offsetTop) {}
     li.forEach(ltx => ltx.classList.remove("active"));
     li[len].classList.add("active");
 }
