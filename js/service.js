24c72dcd041d4a71870d3f4fd83bca69ea46c2c4
Jaeseok <answo93@1thefull.com>
2023-01-29 00:12:57 +0900

    이미지 변경 및 mp4 배경색 이슈 개선 진행중

--- js/service.js
+++ js/service.js
@@ -10,3 +10,40 @@ function activeMenu() {
 
 activeMenu();
 window.addEventListener("scroll", activeMenu);
+
+// 배경색 지정
+function isColorInRange(expectedColor, givenColor) {
+    const THRESHOLD = 40;
+    for (var i = 0; i < 3; i++) {
+        if (((expectedColor[i] - THRESHOLD) > givenColor[i]) 
+         || ((expectedColor[i] + THRESHOLD) < givenColor[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function setVideoBgColor(vid, nativeColor) {
+    if (vid) {
+        var vidBg = vid.parentElement;
+        if (vidBg) {
+            // draw first pixel of video to a canvas
+            // then get pixel color from that canvas
+            var canvas = document.createElement("canvas");
+            canvas.width = 1;
+            canvas.height = 1;
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(vid, 0, 0, 1, 1);
+
+            var p = ctx.getImageData(0, 0, 1, 1).data;
+            //console.log("rgb(" + p[0] + "," + p[1] + "," + p[2] + ")");
+            if (isColorInRange(nativeColor, p)) {        
+                vidBg.style.backgroundColor = "rgb(" + p[0] + "," + p[1] + "," + p[2] + ")";
+            }
+        }
+    }
+}
+
+function setVideoBgColorDelayed(vid, nativeColor) {
+    setTimeout(setVideoBgColor, 100, vid, nativeColor);
+}
