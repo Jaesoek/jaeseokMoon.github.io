a5bb7a2525f0f70c94e34364e2aa0b2b228d3ffc
Jaeseok <answo93@1thefull.com>
2023-01-29 20:44:33 +0900

    - 스크롤 반응하여 Nav 추가하도록 변경 - Nav 클릭시 이동하는 위치 개선 - view_4, 11 애니메이션 튀는 현상 개선

--- js/service.js
+++ js/service.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
+
 const li = document.querySelectorAll(".move");
 const navPos = document.querySelectorAll(".nav_point");
 
@@ -5,16 +7,21 @@ function activeMenu() {
     let len = navPos.length
     while (--len && window.scrollY + 500 < navPos[len].offsetTop) { }
     li.forEach(ltx => ltx.classList.remove("active"));
-    if (len == 0) {
-        if (window.scrollY + 500 < navPos[len].offsetTop) {
-            li[len].classList.add("active");
-        } else {
-            li[len + 1].classList.add("active");
-        }
-    } else {
-        li[len + 1].classList.add("active");
-    }
+    li[len].classList.add("active");
 }
 
 activeMenu();
 window.addEventListener("scroll", activeMenu);
+
+const floatingOn = gsap.to(".floating-menu", { opacity: 1, paused: true });
+ScrollTrigger.create({
+    trigger: ".service_view_6",
+    start: "80% bottom",
+    onEnter: () => floatingOn.play()
+});
+
+ScrollTrigger.create({
+    trigger: ".service_view_6",
+    start: "30% bottom",
+    onLeaveBack: () => floatingOn.pause(0)
+});
